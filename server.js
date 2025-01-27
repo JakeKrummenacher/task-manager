577da2426b078a727889e3a8f55d8703d2cf10e0
Jake Krummenacher <94008106+JakeKrummenacher@users.noreply.github.com>
2025-01-26 20:09:46 -0700

    Fix port number

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import http from 'http'
 
 const app = express()
 const server = http.createServer(app)
-const port = process.env.PORT || 3001;
+const port = process.env.WS_PORT || 3001;
 
 const io = new Server(server, {
   cors: {
